bd92795b7edf777db7e60ac7cd59c647ff10d746
SuXY-O-O <Su19991229@live.com>
2020-12-16 12:56:22 +0800

    finish define model

--- mysite/models.py
+++ mysite/models.py
@@ -68,11 +68,84 @@ class UpdateData(models.Model):
 class Issue(models.Model):
     id = models.AutoField(primary_key=True)
     create_date = models.DateTimeField(auto_now_add=True)
-    create_user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    create_user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    target_update = models.ForeignKey(Update, on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
     answer_date = models.DateTimeField(auto_now=True)
     answer_info = models.TextField()
 
     def __str__(self):
-        return 'issue_' + self.id.__str__() + self.create_date.__str__()
+        return 'issue_' + self.id.__str__() + '_' + self.create_date.__str__()
 
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    create_user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    target_update = models.ForeignKey(Update, on_delete=models.CASCADE)
+    info = models.TextField()
+
+    def __str__(self):
+        return 'comment_' + self.id.__str__() + '_' + self.create_date.__str__()
+
+
+class Apply(models.Model):
+    id = models.AutoField(primary_key=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    create_user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    target_group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    info = models.TextField()
+    status = models.BooleanField(default=False)
+    reply_time = models.DateTimeField(auto_now=True)
+    reply = models.BooleanField(default=False)
+
+    def __str__(self):
+        return 'apply' + self.create_user.__str__() + 'to' + self.target_group.__str__()
+
+
+class Compare(models.Model):
+    id = models.AutoField(primary_key=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    create_user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    upd1 = models.ForeignKey(Update, on_delete=models.CASCADE)
+    upd2 = models.ForeignKey(Update, on_delete=models.CASCADE)
+    info = models.TextField()
+
+    def __str__(self):
+        return 'compare' + self.upd1.__str__() + ' and ' + self.upd2.__str__()
+
+
+class UserToGroup(models.Model):
+    id = models.AutoField(primary_key=True)
+    linked_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    linked_group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.linked_user.__str__() + ' in ' + self.linked_group.__str__()
+
+
+class ExpToGroup(models.Model):
+    id = models.AutoField(primary_key=True)
+    linked_exp = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    linked_group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.linked_exp.__str__() + ' in ' + self.linked_group.__str__()
+
+
+class UpdToExp(models.Model):
+    id = models.AutoField(primary_key=True)
+    linked_exp = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    linked_upd = models.ForeignKey(Update, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.linked_upd.__str__() + ' in ' + self.linked_exp.__str__()
+
+
+class DataToUpd(models.Model):
+    id = models.AutoField(primary_key=True)
+    linked_data = models.ForeignKey(UpdateData, on_delete=models.CASCADE)
+    linked_upd = models.ForeignKey(Update, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.linked_upd.__str__() + ' in ' + self.linked_data.__str__()
