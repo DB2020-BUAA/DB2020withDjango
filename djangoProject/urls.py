67b596403f6263b9d12c1fa84afa2b4429926c77
Zars19 <1036473307@qq.com>
2020-12-23 16:48:39 +0800

    update ignore1

---
+++ djangoProject/urls.py
@@ -0,0 +1,44 @@
+"""djangoProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from mysite import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', views.index, name="main"),
+
+    path('login/<str:info>/<int:i_type>', views.login_page, name='login_redirect'),
+    path('login', views.login_page, name='login'),
+    path('login/sign-in', views.sign_in, name='sign_in'),
+    path('login/sign-out', views.sign_out, name='sign_out'),
+    path('login/sign-up', views.sign_up, name='sign_up'),
+    path('login/register', views.register_page, name='register'),
+    path('login/register/<str:info>/<int:i_type>', views.register_page, name='register_redirect'),
+
+    path('group/<int:group_id>', views.group, name="group"),  # TODO: link to group page
+    path('group-list', views.list_group, name="group_list"),
+    path('group-create', views.create_group, name='group_create'),
+    path('group-list/<str:warning>/<int:w_type>', views.list_group, name="group_list_redirect"),
+
+    path('diff-list', views.list_diff, name="diff_list"),
+    path('diff-list/<str:warning>/<int:w_type>', views.list_diff, name="diff_list_redirect"),
+    path('diff-delete/<int:diff_id>', views.delete_diff, name='diff_delete'),
+]
+
+handler404 = 'mysite.views.page_not_found'
